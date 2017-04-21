450c3d6ea040d4e04bc5dc275076f0c06a6af2cf
Nathan Stilwell <nstilwell@meetup.com>
2017-04-21 12:20:25 -0400

    I wrote this a while ago.
    I think it fixes a problem with counting the number of bells at the half
    hour. Why on earth I was compelled to use a double ternary, I don't
    remember. I really wanted that one liner I guess.

--- index.js
+++ index.js
@@ -1,74 +1,23 @@
-#!/usr/bin/env /usr/local/bin/node
-/*eslint no-console: ["error", { allow: ["warn", "error"] } ] */
-
 const watches = [
-  'middle',
-  'middle',
-  'middle',
-  'middle',
-
-  'morning',
-  'morning',
-  'morning',
-  'morning',
-
-  'forenoon',
-  'forenoon',
-  'forenoon',
-  'forenoon',
-
-  'afternoon',
-  'afternoon',
-  'afternoon',
-  'afternoon',
-
-  'first dog',
-  'first dog',
-  'second dog',
-  'second dog',
-
-  'first',
-  'first',
-  'first',
-  'first'
+  'middle', 'middle', 'middle', 'middle',
+  'morning', 'morning', 'morning', 'morning',
+  'forenoon', 'forenoon', 'forenoon', 'forenoon',
+  'afternoon', 'afternoon', 'afternoon', 'afternoon',
+  'first dog', 'first dog', 'second dog', 'second dog',
+  'first', 'first', 'first', 'first'
 ];
 
 const bells = [
-  8,    // 0
-  2,    // 1
-  4,    // 2
-  6,    // 3
-
-  8,    // 4
-  2,    // 5
-  4,    // 6
-  6,    // 7
-
-  8,    // 8
-  2,    // 9
-  4,    // 10
-  6,    // 11
-
-  8,    // 12
-  2,    // 13
-  4,    // 14
-  6,    // 15
-
-  8,    // 16
-  2,    // 17
-  4,    // 18
-  6,    // 19
-
-  8,    // 20
-  2,    // 21
-  4,    // 22
-  6    // 23
+  8, 2, 4, 6,
+  8, 2, 4, 6,
+  8, 2, 4, 6,
+  8, 2, 4, 6
 ];
 
-export function (hour, minute) {
+module.exports = function (hour, minute) {
   const hour_bells = bells[hour];
   const half_hour_bell = (minute >= 30) ? 1 : 0;
-  const bells_count = (hour_bells + half_hour_bell) % 8;
+  const bells_count = (hour_bells < 8) ? hour_bells + half_hour_bell : (half_hour_bell) ? half_hour_bell : hour_bells;
 
   return {
     bells: bells_count,
